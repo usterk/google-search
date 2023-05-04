f6e7a3647288714c991630aaad40b57a0f0d7d45
Paweł Guć <usterk@gmail.com>
2023-05-04 22:45:26 +0200

    Added google search api

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
-from googlesearch import search
+from googleapiclient.discovery import build
+
+load_dotenv()  # Load the environment variables from the .env file
 
 app = FastAPI()
+
+api_key = os.getenv("GOOGLE_SEARCH_API")
+google_search_service = build("customsearch", "v1", developerKey=api_key)
 
 def pobierz_linki_wynikow(fraza_wyszukiwania):
     linki_wynikow = []
     try:
-        for link in search(fraza_wyszukiwania, num_results=3):
-            linki_wynikow.append(link)
+        response = google_search_service.cse().list(q=fraza_wyszukiwania, cx="017576662512468239146:omuauf_lfve", num=3).execute()
+        for item in response['items']:
+            linki_wynikow.append(item['link'])
     except Exception as e:
         print(f"Błąd podczas wyszukiwania: {e}")
     
     return linki_wynikow
 
-@app.get("/wyszukaj/{fraza}")
-async def wyszukaj(fraza: str):
+@app.get("/google/{fraza}")
+async def google(fraza: str):
     return {"wyniki": pobierz_linki_wynikow(fraza)}
